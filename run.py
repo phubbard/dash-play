9be6e5da16248aab133cf356a33aa13f738fcecc
Paul Hubbard <phubbard@gmail.com>
2015-09-26 15:43:26 -0700

    move to rpi

--- run.py
+++ run.py
@@ -6,18 +6,28 @@ import site
 site.addsitedir("/usr/local/lib/python2.7/site-packages")
 import os
 import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+from datetime import datetime
 
 from scapy.all import *
+
+
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+      d = datetime.now()
       if pkt[ARP].hwsrc == '74:c2:46:a4:4c:e7': # Glad
         print "Pushed Glad"
-        os.system('osascript play-glad.scpt')
+        if d.hour < 12:
+          os.system('osascript play-glad-am.scpt')
+        else:
+          os.system('osascript play-glad-pm.scpt')
       elif pkt[ARP].hwsrc == '74:c2:46:3e:94:78':
         print "Pushed Bounty"
-        os.system('osascript play-bounty.scpt')
+        if d.hour < 12:
+          os.system('osascript play-bounty-am.scpt')
+        else:
+          os.system('osascript play-bounty-pm.scpt')
       else:
         print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
